1d9969076379d34897765e83750e7012246fd519
Gal Oiring <74841666+galoiring@users.noreply.github.com>
2023-06-25 22:25:54 +0300

    Refael branch (#7)
    add currency exchange, and json files for read/write insted/after server call

--- app.js
+++ app.js
@@ -3,103 +3,24 @@ const readline = require("readline");
 const axios = require("axios");
 const Validator = require("jsonschema").Validator;
 const valid = new Validator();
-const skyScannerFlightSchema = require("./skyScannerResponseSchema.json");
+const skyScannerFlightSchema = require("./skyScannerFlightResponseSchema.json");
+const skyScannerHotelSchema = require("./skyScannerHotelResponseSchema.json");
+const fs = require("fs");
+const express = require("express");
+const {convertCurrency} = require('./convertCurrency');
+
 
 const hostname = "127.0.0.1";
 const port = 3001;
 const url = hostname + port;
-const skyScannerflightsUrl =
-  "https://skyscanner-api.p.rapidapi.com/v3/flights/live/search/create";
-const skyScannerHotelstsUrl =
-  "https://skyscanner-api.p.rapidapi.com/v3e/hotels/live/search/create";
-const destinationPlaceMarket = "US";
 
-let originAirportCode = "TLV";
-let destinationAirportCode = "DEL";
 let limit = 4;
-
-//enter 3 char or more for search
-//this one will be converted with the autocomplete to iata airport code for requestConfig
-let searchTerm = "Lond";
+let currencyFromType = "USD";
+let currencyToType = "ILS";
+let exchangeRate;
 
 // JSON format of the request configuration for axios
-const flightRequestConfig = {
-  method: "POST",
-  url: skyScannerflightsUrl,
-  headers: {
-    "Content-Type": "application/json",
-    "X-RapidAPI-Key": "a7e2ee614cmshe7c5245f811fecfp1d32f1jsndd3d0f93ae87",
-    "X-RapidAPI-Host": "skyscanner-api.p.rapidapi.com",
-  },
-  data: {
-    query: {
-      market: "US",
-      locale: "en-GB",
-      currency: "EUR",
-      queryLegs: [
-        {
-          originPlaceId: { iata: originAirportCode },
-          destinationPlaceId: { iata: destinationAirportCode },
-          date: { year: 2023, month: 9, day: 20 },
-        },
-      ],
-      cabinClass: "CABIN_CLASS_ECONOMY",
-      adults: 2,
-      childrenAges: [3, 9],
-    },
-  },
-};
-
-const hotelsRequestConfig = {
-  method: "POST",
-  url: skyScannerHotelstsUrl,
-  headers: {
-    "content-type": "application/json",
-    "X-RapidAPI-Key": "a7e2ee614cmshe7c5245f811fecfp1d32f1jsndd3d0f93ae87",
-    "X-RapidAPI-Host": "skyscanner-api.p.rapidapi.com",
-  },
-  data: {
-    query: {
-      market: destinationPlaceMarket,
-      locale: "en-GB",
-      currency: "GBP",
-      adults: 2,
-      placeId: {
-        entityId: "27539564",
-      },
-      checkInDate: {
-        year: 2023,
-        month: 9,
-        day: 3,
-      },
-      checkOutDate: {
-        year: 2023,
-        month: 9,
-        day: 12,
-      },
-      rooms: 1,
-      childrenAges: [4, 2],
-      sortBy: "RELEVANCE_DESC",
-    },
-  },
-};
-
-const optionsForAutocomplete = {
-  method: "POST",
-  url: "https://skyscanner-api.p.rapidapi.com/v3/autosuggest/flights",
-  headers: {
-    "content-type": "application/json",
-    "X-RapidAPI-Key": "9da54de026msh5bb0867e8c5b75ep1e76aajsnb2ca5fce3c19",
-    "X-RapidAPI-Host": "skyscanner-api.p.rapidapi.com",
-  },
-  data: {
-    query: {
-      market: "UK",
-      locale: "en-GB",
-      searchTerm: searchTerm,
-    },
-  },
-};
+const { flightRequestConfig, hotelsRequestConfig, optionsForAutocomplete } = require('./configVariables');
 
 const makeSearchRequest = async () => {
   try {
@@ -113,68 +34,124 @@ const makeSearchRequest = async () => {
 //TODO: might need to improve the name so it will be the city name but the relevant airport iata Code
 // TODO: ask Refael if full city name needed for the hotels search or iata code is fine.
 const parseSearch = (response) => {
+  const places = response.data.places;
   for (let i = 0; i < limit; ++i) {
-    if (response.data.places[i].type === "PLACE_TYPE_AIRPORT") {
-      const name = response.data.places[0].cityName;
-      const iataCode = response.data.places[i].iataCode;
-      const hirarchy = response.data.places[i].hierarchy;
+    if (places[i].type === "PLACE_TYPE_AIRPORT") {
+      const name = places[0].cityName;
+      const iataCode = places[i].iataCode;
+      const hirarchy = places[i].hierarchy;
       return { name, iataCode, hirarchy };
     }
   }
 };
 
-const askQuestion = (question) => {
-  return new Promise((resolve) => {
-    rl.question(question, (answer) => {
-      resolve(answer);
-    });
+// convert response to JSON and writing to a file
+const writeResponseToJSONFile = (response, fileName) => {
+  fs.writeFile(`${fileName}.json`, JSON.stringify(response, null, "\t"), (error) => {
+    // in case of a writing problem
+    if (error) {
+      console.error(error);
+      throw error;
+    }
+  })
+}
+
+// generic function to read the response from JSON file
+function readRequestFromFile(fileName) {
+  let response = fs.readFileSync(`${fileName}.json`, "utf-8", (error) => {
+    if (error) {
+      console.error(error);
+    }
   });
-};
+  try {
+    response = JSON.parse(response);
+  } catch (err) {
+    console.error(err);
+  }
+  console.log(`successed to read ${fileName}`);
 
-const getUserInput = async () => {
-  const originCode = await askQuestion("Enter the origin airport code: ");
-  const destinationCode = await askQuestion(
-    "Enter the destination airport code: "
-  );
-  const limit = await askQuestion("Enter the limit of flight: ");
+  return response;
+}
 
-  return { originCode, destinationCode, limit };
-};
+// make the request from server and return the response
+const requestFromServer = async (searchResponse) => {
+  flightRequestConfig.data.query.queryLegs[0].originPlaceId.iata =
+    searchResponse.iataCode;
+
+  try {
+    const flightsResponse = await axios.request(flightRequestConfig);
+    const hotelsResponse = await axios.request(hotelsRequestConfig);
+    return { flightsResponse, hotelsResponse }
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// make the request from JSON file and return the response
+const requestFromFile = () => {
+  const flightsResponse = readRequestFromFile('flightResponse');
+  const hotelsResponse = readRequestFromFile('hotelsResponse');
+
+  return { flightsResponse, hotelsResponse }
+}
 
 // make request and send to print
-const makeFlightRequest = async (searchResponse) => {
-  try {
-    // const userInput = await getUserInput();
-    // originAirportCode = userInput.originCode;
-    // destinationAirportCode = userInput.destinationCode;
-    // limit = userInput.limit;
-    //
-    // requestConfig.data.query.queryLegs[0].originPlaceId.iata = originAirportCode;
-    // requestConfig.data.query.queryLegs[0].destinationPlaceId.iata = destinationAirportCode;
-
-    flightRequestConfig.data.query.queryLegs[0].originPlaceId.iata =
-      searchResponse.iataCode;
-
-    const responseFlights = await axios.request(flightRequestConfig);
-    const responseHotels = await axios.request(hotelsRequestConfig);
-    const hotelArr = parsingHotels(responseHotels);
-    const flightsArr = parsingFlights(responseFlights);
-    console.log(hotelArr, flightsArr);
-  } catch (error) {
-    console.error(error);
+const makeRequest = (searchResponse, readFromServer) => {
+  let flightsArr;
+  let hotelArr;
+  if (readFromServer == true) {
+    requestFromServer(searchResponse).then(({ flightsResponse, hotelsResponse }) => {
+      try {
+        flightsArr = parsingFlights(flightsResponse.data);
+        hotelArr = parsingHotels(hotelsResponse.data);
+        writeResponseToJSONFile(flightsResponse.data, 'flightResponse');
+        console.log("successed to write flights response into file")
+        writeResponseToJSONFile(hotelsResponse.data, 'hotelsResponse');
+        console.log("successed to write hotels response into file")
+      } catch (err) {
+        console.log(err + " failed to write or parse");
+      }
+    })
+  } else {
+    const { flightsResponse, hotelsResponse } = requestFromFile();
+    try {
+      flightsArr = parsingFlights(flightsResponse);
+      hotelArr = parsingHotels(hotelsResponse);
+    } catch (err) {
+      {
+        console.log(err + " failed from catch");
+      }
+    }
   }
+
+  return { flightsArr, hotelArr }
 };
 
+// use Validator to validate the response
+const validateResponse = (response, schemaToValid, serverType) => {
+  const responseToValidate = response;
+  const ValidatorResult = valid.validate(responseToValidate, schemaToValid, {
+    throwFirst: true,
+  });
+  console.log(`${serverType} valid: ${ValidatorResult.valid}`);
+}
 // parse the hotels response and print on CLI
-const parsingHotels = (response) => {
-  const hotels = response.data.content.results.hotels;
+function parsingHotels(response) {
+  validateResponse(response, skyScannerHotelSchema, "hotel");
+  const hotels = response.content.results.hotels;
+
+  if (Object.keys(hotels).length == 0) {
+    throw new Error(`hotels is empty`);
+  }
+
   const hotelArr = [];
   for (let i = 0; i < limit; ++i) {
     const key = hotels[i];
     const hotelOption = {
       hotelName: key.name,
-      reate: key.numberOfStars,
-      price: key.priceInfo.price,
+      rate: key.numberOfStars,
+      price: `${key.priceInfo.price}$`,
     };
     console.log();
     console.log(`hotel option ${i + 1}:`);
@@ -187,29 +164,30 @@ const parsingHotels = (response) => {
 
 // parse the flights response and print on CLI
 const parsingFlights = (response) => {
-  const responseToValidate = response.data;
-  if (
-    (valid.validate(responseToValidate, skyScannerFlightSchema, {
-      throwFirst: true,
-    }).valid = false)
-  ) {
-    throw new Error(responseToValidate);
-  }
-  const { legs, itineraries, places } = response.data.content.results;
-  const legsArr = Object.keys(response.data.content.results.legs);
+  validateResponse(response, skyScannerFlightSchema, "flight");
+  const { legs, itineraries, places } = response.content.results;
+  const legsArr = Object.keys(response.content.results.legs);
   const flightsArr = [];
+  try {
+    exchangeRate = convertCurrency();
+    console.log(exchangeRate + "exchangeRate");
+  }catch(err) {
+    exchangeRate = 0.36;
+  }
+
+  if (Object.keys(legs).length == 0 || Object.keys(itineraries).length == 0) {
+    throw new Error(`legs/itineraries is empty, ${response.status}`);
+  }
 
   for (let i = 0; i < limit; ++i) {
     const key = legsArr[i];
     const flightOption = {
-      originName: `${places[legs[key].originPlaceId].name} - ${
-        places[legs[key].originPlaceId].iata
-      }`,
-      destinationName: `${places[legs[key].destinationPlaceId].name} - ${
-        places[legs[key].destinationPlaceId].iata
-      }`,
+      originName: `${places[legs[key].originPlaceId].name} - ${places[legs[key].originPlaceId].iata
+        }`,
+      destinationName: `${places[legs[key].destinationPlaceId].name} - ${places[legs[key].destinationPlaceId].iata
+        }`,
       date: legs[key].departureDateTime,
-      price: itineraries[key].pricingOptions[0].price.amount,
+      price: `${itineraries[key].pricingOptions[0].price.amount / exchangeRate} ${currencyToType}`,
     };
     console.log();
     console.log(`Flight Option ${i + 1}:`);
@@ -220,6 +198,7 @@ const parsingFlights = (response) => {
   return flightsArr;
 };
 
+// init the program, pass true to makeRequest for get response from server, and false for file
 const initializeServerAndGetData = () => {
   const server = http.createServer((req, res) => {
     res.statusCode = 200;
@@ -230,11 +209,9 @@ const initializeServerAndGetData = () => {
   // server.listen(port, hostname, () => {
   //   console.log(`Server running at http://${hostname}:${port}`);
   makeSearchRequest().then((respones) => {
-    makeFlightRequest(respones);
+    makeRequest(respones, false);
   });
 };
-
-makeFlightRequest;
 
 // const server = http.createServer((req, res) => {
 //   res.statusCode = 200;
@@ -246,4 +223,5 @@ makeFlightRequest;
 // });
 
 initializeServerAndGetData();
-// makeSearchRequest()
+
+
